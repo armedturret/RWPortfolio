374ea05b827b8488b5c140057662fb20eb2f337a
Robert White <rjwhitenh@outlook.com>
2023-09-19 14:03:09 -0400

    Moved templates to be more generic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { BioComponent } from './bio/bio.component';
 import { ReferencesComponent } from './references/references.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ReferenceComponent } from './reference/reference.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BioComponent,
     ReferencesComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    HeaderComponent,
+    FooterComponent,
+    ReferenceComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
